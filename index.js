16ecbb0b24f83a094ff6aed008be10d9850b9c34
Sshivamverma009 <shivamverma.sv777@gmail.com>
2024-11-28 14:56:22 +0530

    my Submission

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ import router from "./routes/schoolRoutes.js"
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use('/api', router);
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
