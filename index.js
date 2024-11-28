6e51ef95e453185d23c8d3d73817fbd7aa62d858
Sshivamverma009 <shivamverma.sv777@gmail.com>
2024-11-29 00:07:42 +0530

    update in file index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express"
 import schoolRoutes from "./routes/schoolRoutes.js"
+import cors from "cors"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', schoolRoutes);
 
